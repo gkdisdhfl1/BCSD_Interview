565487a2ebb1e1686656835b2c3c145d766e6efb
심명교 <gkdisdhfl1@naver.com>
2024-02-05 17:40:06 +0900

    홈페이지 생성

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -2,11 +2,11 @@ package com.example.community.controller;
 
 import com.example.community.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
   private final TopicService topicService;
 
@@ -16,8 +16,10 @@ public class HomeController {
   }
 
   @GetMapping(value = {"", "/"})
-  public String topichome(Model model) {
+  public String home(Model model) {
     model.addAttribute("topics", topicService.getAllTopics());
     return "home";
   }
+
+
 }
--- src/main/java/com/example/community/security/SecurityConfig.java
+++ src/main/java/com/example/community/security/SecurityConfig.java
@@ -1,14 +1,14 @@
-package com.example.community.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//package com.example.community.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@Configuration
 //@EnableWebSecurity
